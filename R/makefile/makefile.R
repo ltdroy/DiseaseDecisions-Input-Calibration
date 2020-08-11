f9fbd757a89974bd3d007078e785039a354c995e
LTD <laurence.t.droy@gmail.com>
2020-08-11 10:54:06 +0100

    initial commit, added project skeleton

---
+++ R/makefile/makefile.R
@@ -0,0 +1,16 @@
+### Import dependencies
+library(dplyr)
+library(simplepivot)
+library(ggplot2)
+library(fs)
+library(purrr)
+
+### Poll Input Data
+
+### Import Input Data
+
+### Process Data
+
+### Render Analysis
+
+### Store output data
